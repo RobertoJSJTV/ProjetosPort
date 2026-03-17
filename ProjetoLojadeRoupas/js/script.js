62816f7ed9881c3d97d7434a26fe6d5de7f92911
RobertoJSJTV <Robertojsj18@gmail.com>
2026-03-17 17:54:32 -0300

    updt: modificacoes

--- ProjetoLojadeRoupas/js/script.js
+++ ProjetoLojadeRoupas/js/script.js
@@ -545,7 +545,7 @@ async function carregarDetalhesProduto() {
     const produtoId = urlParams.get('id');
 
     if (!produtoId) {
-        window.location.href = 'lojaRoupas.html';
+        window.location.href = 'LojaRoupas.html';
         return;
     }
 
